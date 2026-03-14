66f058dd2867430d9db3007a871ce4971c390417
Victoria Bondova <victoriabondova9@gmail.com>
2026-03-14 15:49:57 +0200

    Identity Setting Changes

--- UrbanPlantRescueApp/Program.cs
+++ UrbanPlantRescueApp/Program.cs
@@ -1,47 +1,89 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using UrbanPlantRescueApp.Data;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services
-    .AddDefaultIdentity<IdentityUser>(options =>
+namespace UrbanPlantRescueApp
+{
+    public class PRogram
     {
-        options.SignIn.RequireConfirmedAccount = false;
-    })
-    .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddControllersWithViews();
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            
+            builder.Services
+                .AddDefaultIdentity<IdentityUser>(options =>
+                {
+                    ConfigureIdentity(options, builder.Configuration);
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            
+            builder.Services.AddControllersWithViews();
+            
+            WebApplication app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            
+            app.UseHttpsRedirection();
+            
+            app.UseStaticFiles();
+            
+            app.UseRouting();
+            
+            app.UseAuthentication();
+            app.UseAuthorization();
+            
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapRazorPages();
+            
+            app.Run();
+        }
+        private static void ConfigureIdentity(IdentityOptions options, ConfigurationManager configuration)
+        {
+            options.SignIn.RequireConfirmedAccount = configuration
+                .GetValue<bool>("IdentityOptions:SignIn:RequireConfirmedAccount");
+            options.SignIn.RequireConfirmedEmail = configuration
+                .GetValue<bool>("IdentityOptions:SignIn:RequireConfirmedEmail");
+            options.SignIn.RequireConfirmedPhoneNumber = configuration
+                .GetValue<bool>("IdentityOptions:SignIn:RequireConfirmedPhoneNumber");
+            
+            options.User.RequireUniqueEmail = configuration
+                .GetValue<bool>("IdentityOptions:User:RequireUniqueEmail");
 
-WebApplication app = builder.Build();
+            options.Lockout.MaxFailedAccessAttempts = configuration
+                .GetValue<int>("IdentityOptions:Lockout:MaxFailedAccessAttempts");
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(configuration
+                .GetValue<int>("IdentityOptions:Lockout:DefaultLockoutTimeSpanMin"));
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
+            options.Password.RequireDigit = configuration
+                .GetValue<bool>("IdentityOptions:Password:RequireDigit");
+            options.Password.RequireLowercase = configuration
+                .GetValue<bool>("IdentityOptions:Password:RequireLowercase");
+            options.Password.RequireUppercase = configuration
+                .GetValue<bool>("IdentityOptions:Password:RequireUppercase");
+            options.Password.RequireNonAlphanumeric = configuration
+                .GetValue<bool>("IdentityOptions:Password:RequireNonAlphanumeric");
+            options.Password.RequiredLength = configuration
+                .GetValue<int>("IdentityOptions:Password:RequiredLength");
+            options.Password.RequiredUniqueChars = configuration
+                .GetValue<int>("IdentityOptions:Password:RequiredUniqueChars");
+        }
+    }
 }
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
-
-app.Run();
