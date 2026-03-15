126a57c4ac10c325c68e553f1e1eff6d70f8046d
Victoria Bondova <victoriabondova9@gmail.com>
2026-03-15 15:18:21 +0200

    Add PlantViewModel and IPlantService definition

---
+++ UrbanPlantRescueApp/Services/Interfaces/IPlantService.cs
@@ -0,0 +1,9 @@
+﻿namespace UrbanPlantRescueApp.Services.Interfaces
+{
+    using UrbanPlantRescueApp.ViewModels;
+    public interface IPlantService
+    {
+        Task<IEnumerable<PlantViewModel>> GetAllPlantsAsync();
+        Task<PlantViewModel?> GetPlantByIdAsync(int id);
+    }
+}
---
+++ UrbanPlantRescueApp/ViewModels/PlantViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UrbanPlantRescueApp.ViewModels
+{
+    public class PlantViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string CategoryName { get; set; } = null!;
+    }
+}
