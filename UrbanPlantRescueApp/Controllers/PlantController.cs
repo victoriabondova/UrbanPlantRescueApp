56b128ae40c71758707a769ace35d9d069226bc3
Victoria Bondova <victoriabondova9@gmail.com>
2026-03-15 17:20:19 +0200

    Add PlantController and Index view for plants

---
+++ UrbanPlantRescueApp/Controllers/PlantController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrbanPlantRescueApp.Services.Interfaces;
+
+namespace UrbanPlantRescueApp.Controllers
+{
+    public class PlantController : Controller
+    {
+        private readonly IPlantService plantService;
+
+        public PlantController(IPlantService plantService)
+        {
+            this.plantService = plantService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var plants = await plantService.GetAllPlantsAsync();
+            return View(plants);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var plant = await plantService.GetPlantByIdAsync(id);
+            if (plant == null)
+            {
+                return NotFound();
+            }
+            return View(plant);
+        }
+    }
+}
