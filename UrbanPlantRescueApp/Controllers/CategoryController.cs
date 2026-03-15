ea393f4ec7613a309257e863293db2b6aa9bae8f
Victoria Bondova <victoriabondova9@gmail.com>
2026-03-15 15:11:26 +0200

    Add CategoryController and Index view to display categories

---
+++ UrbanPlantRescueApp/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UrbanPlantRescueApp.Services.Interfaces;
+
+namespace UrbanPlantRescueApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categories = await categoryService.GetAllCategoriesAsync();
+            return View(categories);
+        }
+    }
+}
